6326362900625026d9077bdc43c125e98b2e8cd0
Meghdad Hadidi <meghdad.1987@gmail.com>
2018-10-20 19:44:47 +0330

    Finished first configuration of webpack

---
+++ client/config/webpack.config.js
@@ -0,0 +1,70 @@
+/*******************************
+ * Environment and Imports
+ ******************************/
+const environment = process.env.NODE_ENV || "development"
+const devMode = environment === "development"
+
+const autoprefixer = require("autoprefixer")
+const webpack = require("webpack")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+/*******************************
+ * Entry
+ ******************************/
+const entry = {
+    main: ["./src/index.js"]
+}
+
+const output = {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js"
+}
+
+const modules = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"]
+                }
+            }
+        },
+        {
+            test: /\.(sa|sc|c)ss$/,
+            exclude: [/node_modules/],
+            use: [
+                devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                "css-loader",
+                "postcss-loader",
+                "sass-loader"
+            ]
+        }
+    ]
+}
+
+const plugins = [
+    new MiniCssExtractPlugin({
+        filename: devMode ? "[name].css" : "[name].[hash].css",
+        chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    }),
+    new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+]
+
+module.exports = {
+    entry,
+    devServer: {
+        hot: true
+    },
+    devtool: "inline-cheap-module-source-map",
+    output,
+    module: modules,
+    plugins
+}
